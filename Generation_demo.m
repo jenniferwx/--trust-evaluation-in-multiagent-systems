d54023c8ff9c3d3ea8fff442a2b8178767c79b6b
jenniferwx <wxjennifer@gmail.com>
2014-02-17 11:26:18 -0500

    Update Generation_demo.m

--- Generation_demo.m
+++ Generation_demo.m
@@ -6,6 +6,6 @@ alpha = 0.3;   % the parameter for link density
 dh = 0.8;      %  homophily
 attrNoise = 0.2; % noise parameter for the agent's static features
 vocabSize = 10;  % the number of static features (binary feature vector)
-numObjs = 5;    % the maximum number of features can be true (value equals 1)
+numObjs = 5;    % the maximum number of features can be true (i.e. value equals 1)
 
 [Net,Attributes,label] = undirect_synthetic_generator(numNodes,alpha,dh,numLabels,attrNoise,numObjs,vocabSize);
